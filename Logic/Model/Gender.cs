6620111d896e32b23d82d6df5ab0540c79ee5f73
Lemonyep <108149186+Lemonyep@users.noreply.github.com>
2022-12-03 10:37:49 +0300

    Initial commit

---
+++ Logic/Model/Gender.cs
@@ -0,0 +1,28 @@
+﻿namespace Logic.Model;
+
+public class Gender
+{   /// <summary>
+    /// Пол
+    /// Название
+    /// </summary>
+    public string Name { get; set; }
+    /// <summary>
+    /// Создать пол
+    /// </summary>
+    /// <param name="name"></param>
+    /// <exception cref="ArgumentNullException"></exception>
+
+    public Gender(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            throw new ArgumentNullException("Имя пола не может быть пустым или null",nameof(name));
+        }
+        Name = name;
+    }
+
+    public override string ToString()
+    {
+        return Name;
+    }
+}
---
+++ Logic/Model/User.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace Logic.Model;
+
+public class User
+{
+    public string Name { get; }
+    public Gender Gender { get; }
+    public DateTime BirthDate { get; }
+    public double Weight { get; set; }
+    public double Height { get; set; }
+
+    public User(string name, Gender gender, DateTime datetime, double weight, double height)
+    {
+        #region Проверка
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            throw new ArgumentNullException("Имя пользователя не может быть пустым или null.", nameof(name));
+        }
+        if (gender == null)
+        {
+            throw new ArgumentNullException("Пол не может быть null.",nameof(gender));
+        }
+        if (datetime<DateTime.Parse("01.01.1900") || datetime>=DateTime.Now)
+        {
+            throw new ArgumentException("Невозможна дата рождения.",nameof(datetime));
+        }
+        if (weight <= 0)
+        {
+            throw new ArgumentException("Вес не может быть меньше нуля.", nameof(weight));
+        }
+        if (height <= 0)
+        {
+            throw new ArgumentException("Рост не может быть меньше нуля.", nameof(height));
+        }
+        #endregion
+        Gender = gender;
+        Name = name;
+        Weight = weight;
+        Height = height;
+        BirthDate = datetime;
+    }
+    public override string ToString()
+    {
+        return Name;
+    }
+    
+}
